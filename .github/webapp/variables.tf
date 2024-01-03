d13ff6f3861046545a69f3aecd398e9aa12ea0a7
premkadile <55544831+premkadile@users.noreply.github.com>
2024-01-03 11:32:39 -0500

    Create variables.tf

---
+++ .github/webapp/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "name for the resources"
+}
+
+variable "environment" {
+  type        = string
+  description = "environment for the resources"
+}
